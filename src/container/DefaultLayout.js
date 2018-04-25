2efaf2d32d76a1cd7fd47b7a123fd707326c675f
서윤호 <seoyunho@seoyunhoui-MBP>
2018-04-25 13:34:25 +0900

    [MODIFY] react fragment 적용

--- src/container/DefaultLayout.js
+++ src/container/DefaultLayout.js
@@ -39,25 +39,25 @@ class DefaultLayout extends Component {
     switch(location) {
       case '/' : 
         return (
-          <>
+          <React.Fragment>
             <SideMenu 
               subjects = {this.props.studentSubjects} 
               selectedSubject = {this.props.studendSelectedSubject}
               selectSubject = {this.props.selectSubjectByStudent}
             />
             {this.renderModal(state, actions, this.props.studentSelectedAssignment)}
-          </>
+          </React.Fragment>
         )
       case '/admin' : 
         return ( 
-          <>
+          <React.Fragment>
             <SideMenu 
               subjects = {this.props.adminSubjects} 
               selectedSubject = {this.props.adminSelectedSubject}
               selectSubject = {this.props.selectSubjectByAdmin}
             />
             {this.renderModal(state, actions, this.props.adminSelectedAssignment)}
-          </>
+          </React.Fragment>
         )
       default : return;
     }
