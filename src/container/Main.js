8914179f58d23b10b2aa82109405e50909c6d979
서윤호 <seoyunho@seoyunhoui-MBP>
2018-04-27 17:50:29 +0900

    main, assignment section 병합

--- src/container/Main.js
+++ src/container/Main.js
@@ -1,18 +1,103 @@
 import React, { Component } from 'react';
-import AssignmentSection from '../components/main/AssignmentSection';
+import * as actionTypes from '../action/assignment';
+import AssignmentList from '../components/main/AssignmentList';
+import Calendar from '../components/basic/Calendar';
 import { ModalConsumer } from '../context/ModalProvider';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
 import '../css/main.css';
 
-class Main extends Component{
+class MainLayout extends Component {
+  selectEventHandler = (e) => {
+    this.props.modalActions.modalClick(1);
+    this.props.selectAssignmentsByStudent(e);
+  }
   render() {
+    const { selectAssignmentsCard, 
+            assignmentsCardList, 
+            assignments, 
+            selectedDate, 
+            selectDateHandler } = this.props;
+    
     return (
       <div id="main-section">
-        <ModalConsumer>
-          { (modal) => <AssignmentSection modalActions = {modal.actions} />}
-        </ModalConsumer> 
+        <Calendar 
+          selectDateHandler = {selectDateHandler} 
+          selectEventHandler = {this.selectEventHandler} 
+          events = {assignments} 
+          selectedDate = {selectedDate}
+        />
+        <AssignmentList
+          assignments = {assignments}
+          selectAssignmentsCard = {selectAssignmentsCard}
+          assignmentsCardList = {assignmentsCardList}
+        />
       </div>
     )
   }
 }
 
-export default Main;
+class Main extends Component {
+  async componentDidMount() {
+    await this.props.setDateByStudent();
+    this.setAssignmentsCardList();
+  }
+
+  selectDateHandler = (e) => {
+    this.props.selectDateByStudent(e);
+    this.setAssignmentsCardList();
+  }
+  
+  setAssignmentsCardList = () => {
+    const assignments = this.props.assignments;
+    let selectedDate = {
+      start: new Date(this.props.selectedDate.start),
+      end: new Date(this.props.selectedDate.end)
+    }
+    let assignmentList = [];
+
+    assignmentList = assignments.filter((assignment, index) => { 
+      const startDate = new Date(assignment.start);
+      const endDate = new Date(assignment.end);
+
+      if((startDate >= selectedDate.start && startDate <= selectedDate.end) 
+        || (endDate >= selectedDate.start && endDate <= selectedDate.end)
+        || (startDate <= selectedDate.start && endDate >= selectedDate.end )) return true;
+
+      return false;
+    });
+    return this.props.setAssignmentsCardListByStudent(assignmentList);
+  }
+
+  render() {
+    return (
+      <ModalConsumer>
+          { (modal) => (
+            <MainLayout
+              selectAssignmentsByStudent = {this.props.selectAssignmentsByStudent}
+              selectAssignmentsCard = {this.props.selectAssignmentsByStudent}
+              assignmentsCardList = {this.props.assignmentsCardList}
+              selectDateHandler = {this.selectDateHandler}
+              selectedDate = {this.props.selectedDate}
+              assignments = {this.props.assignments}
+              modalActions = {modal.actions}
+            />
+          )}
+      </ModalConsumer> 
+    )
+  }
+}
+  
+const mapStateToProps= (state) => {
+  return {
+    assignments: state.student.assignments[state.student.subjects[state.student.selectedSubject]],
+    assignmentsCardList: state.student.assignmentsCardList,
+    selectedDate: state.student.selectedDate
+  } 
+}
+  
+const mapDispatchToProps= (dispatch) => {
+  return bindActionCreators({ ...actionTypes }, dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Main);
